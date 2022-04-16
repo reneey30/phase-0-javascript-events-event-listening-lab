98a13e023edc086de97f5ed4b804d57636d07782
Irene Njeru <irenenjeru8@gmail.com>
2022-04-16 15:20:06 +0300

    task completed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let input;
+input = document.getElementById("input")
 function addingEventListener() {
+    input.addEventListener('click', event => input.innerText = 'Hello')
 }
+
+addingEventListener()
